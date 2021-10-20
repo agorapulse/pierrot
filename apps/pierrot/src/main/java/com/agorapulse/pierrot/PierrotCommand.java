015cedf0af55debbacc642c69f7cf57afa618c05
Vladimir Orany <vladimir@orany.cz>
2021-10-20 11:12:10 +0200

    disabled file appender which breaks GraalVM generation

--- apps/pierrot/src/main/java/com/agorapulse/pierrot/PierrotCommand.java
+++ apps/pierrot/src/main/java/com/agorapulse/pierrot/PierrotCommand.java
@@ -1,12 +1,8 @@
 package com.agorapulse.pierrot;
 
 import io.micronaut.configuration.picocli.PicocliRunner;
-import io.micronaut.context.ApplicationContext;
-
-import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
-import picocli.CommandLine.Parameters;
 
 @Command(
     name = "pierrot",
