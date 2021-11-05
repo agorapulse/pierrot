473e4c749dfc67480671d218fbaf87d221a4a541
Vladimir Orany <vladimir@orany.cz>
2021-11-05 13:39:23 +0100

    temporary disable maven release, name with version

--- .github/workflows/upload_to_release.sh
+++ .github/workflows/upload_to_release.sh
@@ -5,7 +5,7 @@ function uploadFile() {
     echo "file: $1, name: $2"
     curl \
     -H "Authorization: token $GITHUB_TOKEN" \
-    -H "Content-Type: $(file -b --mime-type $1)" \
+    -H "Content-Type: application/zip" \
     --data-binary @$1 \
     "$UPLOAD_URL?name=$2"
 }
@@ -13,6 +13,8 @@ function uploadFile() {
 set -e
 set -x
 
-uploadFile "$ARTIFACT_DIRECTORY/pierrot-macos-latest.zip" "pierrot-darwin-$RELEASE_VERSION.zip"
-uploadFile "$ARTIFACT_DIRECTORY/pierrot-ubuntu-latest.zip" "pierrot-linux-$RELEASE_VERSION.zip"
-uploadFile "$ARTIFACT_DIRECTORY/pierrot-windows-latest.zip" "pierrot-windows-$RELEASE_VERSION.zip"
+ls -la "$ARTIFACT_DIRECTORY"
+
+uploadFile "$ARTIFACT_DIRECTORY/pierrot-macos-latest.zip" "pierrot-darwin-amd64-$RELEASE_VERSION.zip"
+uploadFile "$ARTIFACT_DIRECTORY/pierrot-ubuntu-latest.zip" "pierrot-linux-amd64-$RELEASE_VERSION.zip"
+uploadFile "$ARTIFACT_DIRECTORY/pierrot-windows-latest.zip" "pierrot-windows-amd64-$RELEASE_VERSION.zip"
