225c12153025cb32d1bf2cda0358a05f753ff8f0
Vladimir Orany <vladimir@orany.cz>
2021-11-05 11:51:35 +0100

    fixed artifacts names

--- .github/workflows/upload_to_release.sh
+++ .github/workflows/upload_to_release.sh
@@ -2,17 +2,17 @@
 #!/usr/bin/env bash
 
 function uploadFile() {
-    echo "file: $1"
+    echo "file: $1, name: $2"
     curl \
     -H "Authorization: token $GITHUB_TOKEN" \
     -H "Content-Type: $(file -b --mime-type $1)" \
     --data-binary @$1 \
-    "$UPLOAD_URL?name=$(basename $1)"
+    "$UPLOAD_URL?name=$2"
 }
 
 set -e
 set -x
 
-uploadFile "$ARTIFACT_DIRECTORY/pierrot-darwin-amd64.zip"
-uploadFile "$ARTIFACT_DIRECTORY/pierrot-linux-amd64.zip"
-uploadFile "$ARTIFACT_DIRECTORY/pierrot-win-amd64.zip"
+uploadFile "$ARTIFACT_DIRECTORY/pierrot-macos-latest.zip" "pierrot-darwin-$RELEASE_VERSION.zip"
+uploadFile "$ARTIFACT_DIRECTORY/pierrot-ubuntu-latest.zip" "pierrot-linux-$RELEASE_VERSION.zip"
+uploadFile "$ARTIFACT_DIRECTORY/pierrot-windows-latest.zip" "pierrot-windows-$RELEASE_VERSION.zip"
